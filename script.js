9b54c79d76463159fa4dbe60298e92cf0f6f8273
joshua levi sizemore <joshlevisize@gmail.com>
2020-01-23 15:15:07 -0500

    Files added via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Pictures from Unsplash
+// http://unsplash.com/
+
+$('body').vegas({
+  overlay: true,
+  transition: 'random', 
+  transitionDuration: 4000,
+  delay: 10000,
+  color: 'red',
+  animation: 'random',
+  animationDuration: 20000,
+  slides: [
+    { src: 'img/1.jpg' },
+    { src: 'img/2.jpg' },
+    { src: 'img/3.jpg' },
+    { src: 'img/4.jpg' },
+    { src: 'img/5.jpg' },
+    { src: 'img/6.jpg' },
+    { src: 'img/7.jpg' },
+    { src: 'img/8.jpg' },
+    { src: 'img/9.jpg' },
+    { src: 'img/10.jpg' },
+    { src: 'img/11.jpg' },
+    { src: 'img/12.jpg' },
+    { src: 'img/13.jpg' },
+    { src: 'img/14.jpg' },
+    { src: 'img/15.jpg' }
+  ]
+});
