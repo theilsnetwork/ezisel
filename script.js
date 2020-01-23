8d0569b96f928c5f7abc2f8f93b9b508639bc80e
joshua levi sizemore <joshlevisize@gmail.com>
2020-01-23 15:19:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Pictures from Unsplash
-// http://unsplash.com/
+// Most pictures from Unsplash
+// https://unsplash.com/
 
 $('body').vegas({
   overlay: true,
@@ -10,20 +10,20 @@ $('body').vegas({
   animation: 'random',
   animationDuration: 20000,
   slides: [
-    { src: 'img/1.jpg' },
-    { src: 'img/2.jpg' },
-    { src: 'img/3.jpg' },
-    { src: 'img/4.jpg' },
-    { src: 'img/5.jpg' },
-    { src: 'img/6.jpg' },
-    { src: 'img/7.jpg' },
-    { src: 'img/8.jpg' },
-    { src: 'img/9.jpg' },
-    { src: 'img/10.jpg' },
-    { src: 'img/11.jpg' },
-    { src: 'img/12.jpg' },
-    { src: 'img/13.jpg' },
-    { src: 'img/14.jpg' },
-    { src: 'img/15.jpg' }
+    { src: './1.jpg' },
+    { src: './2.jpg' },
+    { src: './3.jpg' },
+    { src: './4.jpg' },
+    { src: './5.jpg' },
+    { src: './6.jpg' },
+    { src: './7.jpg' },
+    { src: './8.jpg' },
+    { src: './9.jpg' },
+    { src: './10.jpg' },
+    { src: './11.jpg' },
+    { src: './12.jpg' },
+    { src: './13.jpg' },
+    { src: './14.jpg' },
+    { src: './15.jpg' }
   ]
 });
